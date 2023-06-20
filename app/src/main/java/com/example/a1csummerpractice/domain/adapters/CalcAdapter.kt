317b449604bd3bf1d1975cc572c83f62d5116702
Kowler <failedjock@gmail.com>
2023-06-20 14:38:58 +0300

    finished chart page's main functions

---
+++ app/src/main/java/com/example/a1csummerpractice/domain/adapters/CalcAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.a1csummerpractice.domain.adapters
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.a1csummerpractice.databinding.ItemChartRbBinding
+import com.example.a1csummerpractice.domain.dtypes.Item
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+class CalcAdapter : RecyclerView.Adapter<CalcAdapter.CalcViewHolder>() {
+
+    var data: List<Item> = emptyList()
+        set(newValue) {
+            field = newValue
+            notifyDataSetChanged()
+        }
+
+    class CalcViewHolder(val binding: ItemChartRbBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CalcViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemChartRbBinding.inflate(inflater, parent, false)
+
+        return CalcViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = data.size
+
+    override fun onBindViewHolder(holder: CalcViewHolder, position: Int) {
+        val item = data[position]
+        val context = holder.itemView.context
+
+        with(holder.binding) {
+            rvItemDate.text = "Дата: " + SimpleDateFormat("dd/MM/yyyy").format(Date(item.date*1000))
+            rvItemAdditionalPeriod.text = "Доп.период: " + item.additional_period
+            rvItemStartAmount.text = "Задолжность: " + item.start_amount.toString()
+            rvItemEndAmount.text = "Сумма к оплате: " + item.end_amount.toString()
+            rvItemCalculated.text = "Начислено: " + item.calculated.toString()
+            rvItemPayed.text = "Оплачено: " + item.payed.toString()
+            rvItemPenalties.text = "Пенни: " + item.penalties.toString()
+            rvItemUploadTime.text = "Год: " + item.upload_time.toString()
+            rvItemLastPaymentDate.text =
+                "Дата последней оплаты: " + SimpleDateFormat("dd/MM/yyyy").format(Date(item.last_payment_date*1000))
+            rvItemCurrentDate.text = "Текущая дата: " + SimpleDateFormat("dd/MM/yyyy").format(Date(item.currentDate*1000))
+            rvItemCorrection.text = "Коррекция: " + item.correction.toString()
+        }
+    }
+}
--- app/src/main/java/com/example/a1csummerpractice/ui/chart/ChartFragment.kt
+++ app/src/main/java/com/example/a1csummerpractice/ui/chart/ChartFragment.kt
@@ -1,18 +1,20 @@
 package com.example.a1csummerpractice.ui.chart
 
-import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.a1csummerpractice.R
 import com.example.a1csummerpractice.data.mappers.MyXAxis
 import com.example.a1csummerpractice.data.mappers.jsonMapper
 import com.example.a1csummerpractice.data.mappers.jsonToEntry
 import com.example.a1csummerpractice.databinding.FragmentChartBinding
+import com.example.a1csummerpractice.domain.adapters.CalcAdapter
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.LineData
@@ -22,6 +24,8 @@ import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 class ChartFragment : Fragment() {
 
     private var _binding: FragmentChartBinding? = null
+    private lateinit var _mainAdapter: CalcAdapter
+    private lateinit var _capAdapter: CalcAdapter
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -36,19 +40,43 @@ class ChartFragment : Fragment() {
             ViewModelProvider(this).get(ChartViewModel::class.java)
         _binding = FragmentChartBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        val chart:LineChart = binding.chart
+        val chart: LineChart = binding.chart
+        val rvMain: RecyclerView = binding.rvChartMain
+        val rvCap: RecyclerView = binding.rvChartCap
 
         val jsonDT = context?.let { jsonMapper(context = it) }
         chart.xAxis.position = XAxis.XAxisPosition.BOTTOM
         chart.xAxis.valueFormatter = MyXAxis()
 
+        //LineDataSet styling
         val lineDataSet: LineDataSet = LineDataSet(jsonDT?.let { jsonToEntry(it) },"Динамика изменения суммы начислений")
+        lineDataSet.lineWidth = 6F
+        lineDataSet.color = R.color.purple_500
+        lineDataSet.mode = LineDataSet.Mode.HORIZONTAL_BEZIER
+        lineDataSet.setCircleColor(Color.BLUE)
+        lineDataSet.setDrawFilled(true)
+        lineDataSet.fillColor = R.color.purple_500
+        lineDataSet.fillAlpha = 190
+        lineDataSet.circleRadius = 7f
+        lineDataSet.valueTextSize = 10f
+
         val dataSets: MutableList<ILineDataSet> = arrayListOf()
         dataSets.add(lineDataSet)
         val data: LineData = LineData(dataSets)
         chart.data = data
         chart.invalidate()
 
+        _mainAdapter = CalcAdapter()
+        _mainAdapter.data = jsonDT!!.calculations.get(0).items
+        _capAdapter = CalcAdapter()
+        _capAdapter.data = jsonDT.calculations.get(1).items
+
+        val layManagerMain = LinearLayoutManager(context)
+        val layManagerCap = LinearLayoutManager(context)
+        rvMain.layoutManager = layManagerMain
+        rvMain.adapter = _mainAdapter
+        rvCap.layoutManager = layManagerCap
+        rvCap.adapter = _capAdapter
 
         return root
     }
